82ad20616a03ecc3fea2c9a13dcaf4b25c8daff7
lindsay.cowell@utsouthwestern.edu <lindsay.cowell@utsouthwestern.edu@92b75ed2-ff87-11dd-9d1a-b59b2e1864b6>
2010-11-07 20:40:08 +0000

    add OGMS to add-to-external.pl

--- trunk/src/tools/add-to-external.pl
+++ trunk/src/tools/add-to-external.pl
@@ -25,6 +25,7 @@ if (!$branchpath)
 
 my $externals_table = 
     [["PATO", "http://purl.org/obo/owl/PATO#PATO_", "http://purl.org/obo/owl/PATO", "\\d{7}","PATO:0001879"],
+     ["OGMS", "http://purl.obolibrary.org/obo/OGMS_", "http://purl.obolibrary.org/obo/ogms.owl", "\\d{7}", "OGMS:0000087"],
      ["OBI", "http://purl.obolibrary.org/obo/OBI_", "http://purl.obolibrary.org/obo/obi.owl", "\\d{7}", "OBI:0100026"],
      ["PRO", "http://purl.org/obo/owl/PRO#PRO_", "http://purl.org/obo/owl/PRO","\\d{9}","PRO:000001958"],
      ["FMA", "http://purl.org/obo/owl/FMA#FMA_", "http://purl.org/obo/owl/FMA","\\d+","FMA:7203"],
