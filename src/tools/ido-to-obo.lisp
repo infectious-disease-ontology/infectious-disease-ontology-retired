c7f8684af29cc8e6f6f7d23354f21a5d525f5bde
alanruttenberg <alanruttenberg@92b75ed2-ff87-11dd-9d1a-b59b2e1864b6>
2010-12-05 22:59:33 +0000

    bug fixes

--- src/tools/ido-to-obo.lisp
+++ src/tools/ido-to-obo.lisp
@@ -1,8 +1,8 @@
-# This is a patched together conversion script, relying on a modified version of the OBI to OBO script lightly edited to work with owlapiv3
+; This is a patched together conversion script, relying on a modified version of the OBI to OBO script lightly edited to work with owlapiv3
 
 (defun ido-dont-include (uri)
   "return t to mean include this term"
-  (let ((matches (or
+  '(let ((matches (or
 		  (#"matches" (uri-full uri) ".*OGMS.*")
 		  (#"matches" (uri-full uri) ".*IAO_.*")
 		  (#"matches" (uri-full uri) "http://www.geneontology.org/formats.*"))
@@ -11,7 +11,7 @@
 	     (member uri `(,!obo:OGMS_0000063 ,!obo:OGMS_0000031 ,!obo:OGMS_0000045)))
 	(not matches))))
 
-(defun generate-ido-obo (&key (kb  (load-ontology "ido:ido-core;ido.owl"))
+(defun generate-ido-obo (&key (ido  (load-ontology "ido:ido-core;ido.owl"))
 			 (path (translate-logical-pathname "ido:ido-core;ido.obo"))
 			 (saved-by "generate-ido-obo")
 			 (default-namespace "IDO")
@@ -20,7 +20,7 @@
 		     ) 
   (instantiate-reasoner ido :pellet-sparql)
   (setf (gethash !owl:Thing (rdfs-labels ido)) '("Thing"))
-  (generate-obo :kb kb :path path :saved-by saved-by :default-namespace default-namespace
+  (generate-obo :kb ido :path path :saved-by saved-by :default-namespace default-namespace
 		:ontology-name ontology-name :ontology-uri ontology-uri :default-definition-source " [OBO:sourced \"IDO consortium http://www.infectiousdiseaseontology.org\"]" :filter-dont-include 'ido-dont-include :include-obsolete t))
 
 (defun rdfs-comments (kb)
